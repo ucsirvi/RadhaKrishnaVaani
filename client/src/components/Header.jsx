67f3ae9df501b5ebf1e4cd72a8299cdd5fe84fb7
Umesh Choudhary <166589058+ucsirvi@users.noreply.github.com>
2024-10-27 01:24:50 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,13 @@
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/1.png"; // Import your logo
 import krishna from "../assets/krishna.jpg"; // Background image
-import { UserButton, useUser } from "@clerk/clerk-react";
+
 
 const Header = () => {
   const { isSignedIn } = useUser();
   const navigate = useNavigate();
 
-  const handleLinkClick = (event, path) => {
-    if (!isSignedIn) {
-      event.preventDefault(); // Prevent navigation
-      navigate("/auth/sign-in"); // Redirect to the sign-in page
-    } else {
-      navigate(path);
-    }
-  };
-
+ 
   return (
     <header className="bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 shadow-lg relative py-2 text-white">
       <div
@@ -39,7 +31,6 @@ const Header = () => {
           <li>
             <Link
               to="/chapters"
-              onClick={(e) => handleLinkClick(e, "/chapters")}
               className="text-pink-100 hover:text-pink-300 transition duration-300 font-semibold tracking-wide"
             >
               Divine Dialogues
@@ -48,7 +39,6 @@ const Header = () => {
           <li>
             <Link
               to="/reels"
-              onClick={(e) => handleLinkClick(e, "/reels")}
               className="text-purple-100 hover:text-purple-300 transition duration-300 font-semibold tracking-wide"
             >
               Divine Melodies
@@ -57,23 +47,12 @@ const Header = () => {
           <li>
             <Link
               to="/images"
-              onClick={(e) => handleLinkClick(e, "/images")}
               className="text-teal-100 hover:text-teal-300 transition duration-300 font-semibold tracking-wide"
             >
               Divine Visions
             </Link>
           </li>
         </ul>
-
-        {isSignedIn ? (
-          <UserButton />
-        ) : (
-          <Link to="/auth/sign-in">
-            <button className="bg-teal-500 px-4 py-2 rounded text-white hover:bg-teal-600 transition duration-300 shadow-md">
-              Sign In
-            </button>
-          </Link>
-        )}
       </nav>
     </header>
   );
